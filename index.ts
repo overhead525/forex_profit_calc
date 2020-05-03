7a09a9bb19590399a915c962d55061fdec812cd6
rbcousar <ritchel.cousar@lifeomic.com>
2020-05-03 16:32:42 -0400

    Added functionality to do many simulations of a trade season

--- index.ts
+++ index.ts
@@ -1,11 +1,11 @@
 const Probability: any = require('./Probability');
 
-let portfolio: number = 300;
+let portfolio: number = 150;
 const originalPortfolio: number = portfolio;
 const risk: number = 0.01;
-const reward: number = 0.03;
-const winRate: string = '70%';
-const lossRate: string = '30%';
+const reward: number = 0.05;
+const winRate: string = '55%';
+const lossRate: string = '45%';
 const findBreakEvenRate = (risk: number, reward: number): string => {
     const raw: number = risk / (risk + reward) * 100;
     return `${raw}%`
@@ -22,6 +22,10 @@ Break-Even Rate = ${breakEvenRate}
 
 Simulating ${numOfTrades} trades...
 `)
+
+const averageNumArray = (arrNums: Array<number>) => {
+    return arrNums.reduce((a, b) => (a + b)) / arrNums.length;
+}
 
 const payout = (currentPortfolio: number, tradeOutcome: Boolean): number => {
     if (tradeOutcome === true) {
@@ -42,14 +46,46 @@ const decideOutcome: any = new Probability({
     }
 });
 
-for (let i = 1; i <= numOfTrades; i++) {
-    let outcome = decideOutcome();
-    let earned = payout(portfolio, outcome);
-    portfolio += earned;
-    console.log(`Portfolio after trade #${i}: $${parseFloat(portfolio.toPrecision(10))} ${outcome ? 'Win' : 'Loss'} ${outcome ? '+' : '-'}$${Math.abs(parseFloat(earned.toPrecision(10)))}`);
+const resultsArray: Array<number> = [];
+const growthArray: Array<number> = [];
+
+const simulateXTrades = (): void => {
+    for (let i = 1; i <= numOfTrades; i++) {
+        let outcome: boolean = decideOutcome();
+        let earned: number = payout(portfolio, outcome);
+        portfolio += earned;
+        // console.log(`Portfolio after trade #${i}: $${parseFloat(portfolio.toPrecision(10))} ${outcome ? 'Win' : 'Loss'} ${outcome ? '+' : '-'}$${Math.abs(parseFloat(earned.toPrecision(10)))}`);
+    }
+    
+    const growth: number = (portfolio - originalPortfolio) / originalPortfolio;
+
+    const finalPortfolio = parseFloat(portfolio.toPrecision(10));    
+    // console.log(`\nFinal portfolio: $${finalPortfolio}`);
+    resultsArray.push(finalPortfolio);
+    
+    const neatGrowth = parseFloat(growth.toPrecision(6)) * 100;
+    // console.log(`\n% growth after ${numOfTrades} trades: ${parseFloat(growth.toPrecision(6)) * 100}%`);
+    growthArray.push(neatGrowth);
+
+    // RESETS
+    portfolio = originalPortfolio;
 }
 
-const growth = (portfolio - originalPortfolio) / originalPortfolio;
+const getSimulations = (numSimulations: number): void => {
+    for (let i = 1; i <= numSimulations; i++) {
+        simulateXTrades();
+    }
 
-console.log(`\nFinal portfolio: $${parseFloat(portfolio.toPrecision(10))}`);
-console.log(`\n% growth after ${numOfTrades} trades: ${parseFloat(growth.toPrecision(6)) * 100}%`);
+    const averageFinalPortfolio: number = averageNumArray(resultsArray);
+    const averageGrowth: number = averageNumArray(growthArray);
+
+    console.log(`
+    After ${numSimulations} simulations of ${numOfTrades} trades, you had
+    - an average final portfolio of $${averageFinalPortfolio};
+    - an average growth of ${averageGrowth}%
+    `);
+
+    console.log('END');
+}
+
+getSimulations(1000000);
